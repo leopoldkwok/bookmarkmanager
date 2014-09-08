bdffcf3881ceacc194740f086f7fc4721bed3caf
leopoldkwok <leopoldkwok@gmail.com>
2014-09-08 18:56:25 +0100

    adding links

--- server.rb
+++ server.rb
@@ -22,3 +22,10 @@ get '/' do
   @links = Link.all
   erb :index
 end
+
+post '/links' do
+  url = params["url"]
+  title = params["title"]
+  Link.create(:url => url, :title => title)
+  redirect to('/')
+end
