7c57872234791e66f30017e3f69f4dcb1099f7f8
leopoldkwok <leopoldkwok@gmail.com>
2014-09-10 18:18:12 +0100

    Adding user accounts - Signing in

--- app/server.rb
+++ app/server.rb
@@ -41,6 +41,22 @@ get '/users/new' do
   erb :"users/new"
 end
 
+get '/sessions/new' do
+  erb :"sessions/new"
+end
+
+post '/sessions' do
+  email, password = params[:email], params[:password]
+  user = User.authenticate(email, password)
+  if user
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    flash[:errors] = ["The email or password is incorrect"]
+    erb :"sessions/new"
+  end
+end
+
 post '/users' do 
   # we just initialize the object
   # without saving it. It may be invalid
